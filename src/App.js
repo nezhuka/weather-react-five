55ad872aa7276f424e70bdb84df7aa54e168ac40
nezhuka <ingasom63@gmail.com>
2023-09-12 15:52:25 +0100

    Coded Wether.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
