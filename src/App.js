ac906d04d8b71317c711da5c1f2b873c2ff32267
nezhuka <ingasom63@gmail.com>
2023-09-12 13:18:33 +0100

    Added repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.shecodes.io/graduates/80008-inha-somsikova" target="_blank">
+          Inha Somsikova
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/nezhuka/weather-react-five" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
